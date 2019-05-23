06c956f6f6bbd39175aacb48a4205ef2bc777a5c
Brian Sekwakwa <briansenpai14@gmail.com>
2019-05-23 16:20:49 +0200

    Updates

---
+++ precache-manifest.d503d80d582ab3b6efc056d33c2bd861.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "949e905c7b90ab84a41863a59dadcf5a",
+    "url": "/weather-today/index.html"
+  },
+  {
+    "revision": "c0a0a4e95ff00cdb65da",
+    "url": "/weather-today/static/css/main.c37b9b53.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/weather-today/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "c0a0a4e95ff00cdb65da",
+    "url": "/weather-today/static/js/main.1e03b7e0.chunk.js"
+  },
+  {
+    "revision": "878550be477c332df42c",
+    "url": "/weather-today/static/js/runtime~main.b06225ba.js"
+  },
+  {
+    "revision": "d652a16b929c67ab057e3f6ce0f343db",
+    "url": "/weather-today/static/media/clouds.d652a16b.jpg"
+  }
+]);
